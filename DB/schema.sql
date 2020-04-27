019da359c5db6063e1c35f176767136985b799ab
Donna Johnson <tjndlc@comcast.net>
2020-04-27 08:21:37 -0700

    creation of folders and files

---
+++ DB/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+
+	id Int( 11 ) NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured boolean,
+	PRIMARY KEY(id)
+);	
---
+++ DB/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bison burger', false);
+
+
