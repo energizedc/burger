80dfd6c7a4d9315dcd16f14266e2231e17969ae8
Donna Johnson <tjndlc@comcast.net>
2020-04-29 09:52:23 -0700

    change connect to connect sql to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "S0cc3r22@@",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("Mysql connected as id " + connection.threadId);
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "S0cc3r22@@",
+    database: "burgers_db"
+  });
+};
+// Make connection.
+connection.connect();
 
 module.exports = connection;
+
