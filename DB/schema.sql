2edd0c3d5fe97d8da41a4809c39b9d1afecd0315
Donna Johnson <tjndlc@comcast.net>
2020-04-28 19:38:59 -0700

    added js, config, routes and html

--- DB/schema.sql
+++ DB/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE burgers (
 
 	id Int( 11 ) NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-	devoured boolean,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY(id)
 );	
--- server.js
+++ server.js
@@ -1,1 +1,35 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+//////    setups up express     ////////////////////////////////
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing    ///////////
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+////   setup up the routes here       /////////////////////////////
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+/////////////////////////////////////////////////////////////////
+//     Listen on port 8080
+/////////////////////////////////////////////////////////////////
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("HI   Server listening on: http://localhost:" + PORT);
+  });
+  
